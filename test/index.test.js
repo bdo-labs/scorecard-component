11bbfb3b22bfd5eb5cf51ad052bc890e99d6e6e8
Péter Henrik Gombos <peter@gombos.no>
2014-07-28 10:22:41 +0200

    Remove summary card from scorecard
    As the summary is a summary of both the scorecard and the tasks, it
    doesn't make sense to have it in scorecard.

--- test/index.test.js
+++ test/index.test.js
@@ -23,7 +23,7 @@ describe('scorecardView', function(){
 
 
 	function compileDirective(tpl){
-		if (!tpl) tpl = '<div scorecard-view scorecard-type="\'summary\'" scorecard-id="2"></div>';
+		if (!tpl) tpl = '<div scorecard-view scorecard-type="\'full\'" scorecard-id="2"></div>';
 
 		inject(function ($compile) {
 			el = $compile(tpl)(scope)[0];
@@ -42,7 +42,7 @@ describe('scorecardView', function(){
 		});
 
 		it('should choose the right scorecard type', function () {
-			expect(el.firstElementChild.classList.contains('scorecard-summary')).toBe(true);
+			expect(el.firstElementChild.classList.contains('scorecard-full')).toBe(true);
 		});
 
 		it('should note the scorecardId', function () {
